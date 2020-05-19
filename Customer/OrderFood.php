7a8c1eff772f53d2d7f0ece12fe720c9702200a3
SutopaBiswas <60354707+SutopaBiswas@users.noreply.github.com>
2020-05-19 13:27:26 +0600

    Add files via upload

---
+++ Customer/OrderFood.php
@@ -0,0 +1,36 @@
+<?php	
+	session_start();
+	if(!isset($_COOKIE['username'])){  
+		header("location: LogIn.php");
+	}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Order Food</title>
+</head>
+<body>
+	<center>
+	<h1> Food Delivery</h1>
+	<h2> Order Food</h2>
+	<form method="POST" action="OrderFood.php">
+		<table>
+		<tr>
+		<td>Food Code </td>
+		<td><input type="number" name="orfood" value=""/></td><br>
+		</tr>
+		<tr>
+		<td>Food Details: </td>
+		<td><input type="text" name="fdetails" value=""/></td><br>
+		</tr>
+		<tr>
+		<td colspan="2"><center><input type="submit" name="submit" value="Add">
+		                        <input type="submit" name="submit" value="Delete"></center></td>
+		</tr>
+		<tr>
+		<td colspan="2"><center><a href=Home.php >Go Home</a></center></td>
+		</tr>
+</body>
+</html>
